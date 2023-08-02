0a59141a26c32f23587df78c39954893ec6158e9
Smit <smitvd2002@gmail.com>
2023-08-02 20:06:41 +0530

    created a different ui design adding checkbox and also learned about border , border radius

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_main);
+
+
+        login = findViewById(R.id.btn);
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(MainActivity.this, "login successfully", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
